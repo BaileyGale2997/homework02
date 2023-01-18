40530ea03d6e0384c4bb2dd4d331576afa88f68a
Bailey Edwards <baileygale2997@gmail.com>
2023-01-17 22:44:29 -0800

    commit3

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ function writeDate() {
         weekday: 'long',
         month: 'long',
         day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
         hour12: true
     });
     const dateEl = document.getElementById('date');
     dateEl.textContent = formatter.format(today);
 }
 writeDate();
+    
